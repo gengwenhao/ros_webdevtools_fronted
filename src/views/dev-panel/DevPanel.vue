1ad2420742c3395cd73a073a3cd92792cb4294a4
gengwenhao97@126.com <gengwenhao97@126.com>
2021-12-07 19:58:12 +0800

    修改了字段名称

--- src/views/dev-panel/DevPanel.vue
+++ src/views/dev-panel/DevPanel.vue
@@ -401,9 +401,10 @@ export default {
             this.appendDummyInput()
                 .appendField(data.name);
             this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
             this.setColour(230);
-            this.setTooltip("");
-            this.setHelpUrl("");
+            this.setTooltip(data.name);
+            this.setHelpUrl(data.name);
           }
         }
       })
